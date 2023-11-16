a56f4080e1109a78679e6111724db9802f5b6491
kwizera-bonheur123 <kbonheur123@gmail.com>
2023-11-16 23:09:20 +0200

    add likes

--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -2,6 +2,7 @@ import { createpost, selectpost, deletepost, selectById, updatepost,addComment }
 import Authorization from "../middleware/Aunthentication";
 import express from "express";
 import fileUpload from "../helper/multer";
+import { addLike } from "../controller/postController";
 
 const postRoutes = express.Router();
 
@@ -11,6 +12,6 @@ postRoutes.get("/select", selectpost);
 postRoutes.get("/selectById/:id", selectById);
 postRoutes.put("/update/:id",fileUpload.single("postImage"), updatepost );
 postRoutes.post("/comment/:id", Authorization, fileUpload.single("postImage"), addComment);
-
+postRoutes.post("/like/:id", Authorization,fileUpload.single("postImage"), addLike);
 
 export default postRoutes
